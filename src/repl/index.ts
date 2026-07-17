b89bccd2047d02fb665f68d581e6cf19e555a26c
Srivishal Sivasubramanian <srivishal.sivasubramanian@gmail.com>
2026-07-17 17:05:20 +0000

    chore: replace legacy repl with tui

--- src/repl/index.ts
+++
@@ -1,3 +0,0 @@
-export { FlueProcess, type FlueEvent, type FlueResult, type PromptCallbacks } from './ipc';
-export { EventRenderer } from './render';
-export { Repl, type ReplOptions, startRepl } from './repl';
