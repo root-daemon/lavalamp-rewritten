73f6cf82918882436091f4bff9f4fff4b51e7657
Srivishal Sivasubramanian <srivishal.sivasubramanian@gmail.com>
2026-07-18 03:06:22 +0000

    chore: new banner and deployment

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,6 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
-export default defineConfig({ integrations: [react()] });
+export default defineConfig({
+  output: 'server',
+  adapter: cloudflare(),
+  integrations: [react()],
+});
